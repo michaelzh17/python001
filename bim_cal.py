a67c645968b9146504f068818cdf3c71bbaf5646
michaelzh17 <macalzhang@gmail.com>
2017-10-11 20:08:44 -0400

    update bim_cal file

---
+++ bim_cal.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+height = 1.75
+weight = 80.5
+
+bmi = weight/height**2
+if bmi < 18.5:
+    print('过轻')
+elif 18.5 < bmi < 25:
+    print('正常')
+elif 25 < bmi < 28:
+    print('过重')
+elif 28 <= bmi <= 32:
+    print('肥胖')
+else: 
+    print ('严重肥胖')
