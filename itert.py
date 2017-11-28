8f7ce6d0a9a673a754e623ec5d991539e23adc11
michaelzh17 <macalzhang@gmail.com>
2017-11-28 12:18:09 -0500

    update itert.py

--- itert.py
+++ itert.py
@@ -8,20 +8,18 @@ def pi(N):
     lst = itertools.count(1, 2)
     # step 2: 取该序列的前N项: 1, 3, 5, 7, 9, ..., 2*N-1.
     ls = list(itertools.takewhile(lambda x:(x+1)//2 <= N, lst))
-    #print(list(ls))
     # step 3: 添加正负符号并用4除: 4/1, -4/3, 4/5, -4/7, 4/9, ...
-    ls_m = list(itertools.takewhile(lambda y: -y, ls))
-    print(ls_m)
+    ls_m = list(map(lambda x: 4/((-1)*x) if x//2%2==1 else 4/x, ls))
     # step 4: 求和:
     return sum(ls_m)
 
 
 print(pi(10))
 print(pi(100))
-#print(pi(1000))
-#print(pi(10000))
+print(pi(1000))
+print(pi(10000))
 assert 3.04 < pi(10) < 3.05
 assert 3.13 < pi(100) < 3.14
-#assert 3.140 < pi(1000) < 3.141
-#assert 3.1414 < pi(10000) < 3.1415
+assert 3.140 < pi(1000) < 3.141
+assert 3.1414 < pi(10000) < 3.1415
 print('ok')
